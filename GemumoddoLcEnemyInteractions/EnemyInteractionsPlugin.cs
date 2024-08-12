78f348272c967c911a60085698f02d757960455c
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2024-08-12 13:49:07 -0400

    Update EnemyInteractionsPlugin.cs

--- GemumoddoLcEnemyInteractions/EnemyInteractionsPlugin.cs
+++ GemumoddoLcEnemyInteractions/EnemyInteractionsPlugin.cs
@@ -15,7 +15,7 @@ public class EnemyInteractionsPlugin : BaseUnityPlugin
 {
     public const string ModGuid = "com.gemumoddo.enemyinteractions";
     public const string ModName = "Enemy Interactions";
-    public const string ModVersion = "1.1.5";
+    public const string ModVersion = "1.1.6";
     public static EnemyInteractionsPlugin instance;
     private void Awake()
     {
