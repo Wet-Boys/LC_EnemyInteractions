b2046144e033f27245e0453052423c85fb4b75cd
huluoboya3 <93986295+huluoboya3@users.noreply.github.com>
2025-03-14 14:53:31 +0800

    Update CanEmoteChecker.cs

--- GemumoddoLcEnemyInteractions/Utils/CanEmoteChecker.cs
+++ GemumoddoLcEnemyInteractions/Utils/CanEmoteChecker.cs
@@ -1,7 +1,4 @@
-﻿using EnemyInteractions.DataStuffs;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using EnemyInteractions.DataStuffs;
 
 namespace EnemyInteractions.Utils
 {
@@ -9,123 +6,48 @@ namespace EnemyInteractions.Utils
     {
         public static bool CanEmote(string enemyName)
         {
-            switch (enemyName)
+            return enemyName switch
             {
-                case "Bunker Spider":
-                    return  EnemyInteractionSettings.bunkerSpiderEmote.Value;
-                    break;
-                case "Hoarding bug":
-                    return  EnemyInteractionSettings.hoardingBugEmote.Value;
-                    break;
-                case "Earth Leviathan":
-                    return  EnemyInteractionSettings.earthLeviathanEmote.Value;
-                    break;
-                case "Crawler":
-                    return  EnemyInteractionSettings.thumperEmote.Value;
-                    break;
-                case "Blob":
-                    return  EnemyInteractionSettings.hygrodereEmote.Value;
-                    break;
-                case "Centipede":
-                    return  EnemyInteractionSettings.snareFleaEmote.Value;
-                    break;
-                case "Nutcracker":
-                    return  EnemyInteractionSettings.nutcrackerEmote.Value;
-                    break;
-                case "Baboon hawk":
-                    return  EnemyInteractionSettings.baboonHawkEmote.Value;
-                    break;
-                case "Puffer":
-                    return  EnemyInteractionSettings.sporeLizardEmote.Value;
-                    break;
-                case "Spring":
-                    return  EnemyInteractionSettings.coilHeadEmote.Value;
-                    break;
-                case "Jester":
-                    return  EnemyInteractionSettings.jesterEmote.Value;
-                    break;
-                case "Flowerman":
-                    return  EnemyInteractionSettings.brackenEmote.Value;
-                    break;
-                case "Girl":
-                    return  EnemyInteractionSettings.ghostGirlEmote.Value;
-                    break;
-                case "MouthDog":
-                    return  EnemyInteractionSettings.eyelessDogEmote.Value;
-                    break;
-                case "ForestGiant":
-                    return  EnemyInteractionSettings.forestKeeperEmote.Value;
-                    break;
-                case "Masked":
-                    return  EnemyInteractionSettings.maskedEmote.Value;
-                    break;
-                case "Shy guy":
-                    return  EnemyInteractionSettings.shyGuyEmote.Value;
-                    break;
-                case "SkibidiToilet":
-                    return  EnemyInteractionSettings.skibidiToiletEmote.Value;
-                    break;
-                case "Demogorgon":
-                    return  EnemyInteractionSettings.demoGorgonEmote.Value;
-                    break;
-                case "Peeper":
-                    return  EnemyInteractionSettings.peeperEmote.Value;
-                    break;
-                case "RadMech":
-                    return  EnemyInteractionSettings.radMechEmote.Value;
-                    break;
-                case "Butler":
-                    return  EnemyInteractionSettings.butlerEmote.Value;
-                    break;
-                case "Tulip Snake":
-                    return  EnemyInteractionSettings.tulipSnakeEmote.Value;
-                    break;
-                case "HarpGhost":
-                    return  EnemyInteractionSettings.harpGhostEmote.Value;
-                    break;
-                case "EnforcerGhost":
-                    return  EnemyInteractionSettings.enforcerGhostEmote.Value;
-                    break;
-                case "BagpipeGhost":
-                    return  EnemyInteractionSettings.bagpipeGhostEmote.Value;
-                    break;
-                case "SlendermanEnemy":
-                    return  EnemyInteractionSettings.slendermanEmote.Value;
-                    break;
-                case "RedWoodGiant":
-                    return  EnemyInteractionSettings.redWoodGiantEmote.Value;
-                    break;
-                case "DriftWoodGiant":
-                    return  EnemyInteractionSettings.driftWoodGiantEmote.Value;
-                    break;
-                case "Foxy":
-                    return  EnemyInteractionSettings.foxyEmote.Value;
-                    break;
-                case "The Fiend":
-                    return  EnemyInteractionSettings.theFiendEmote.Value;
-                    break;
-                case "Siren Head":
-                    return  EnemyInteractionSettings.sirenHeadEmote.Value;
-                    break;
-                case "Football":
-                    return  EnemyInteractionSettings.footballEmote.Value;
-                    break;
-                case "Sentinel":
-                    return  EnemyInteractionSettings.sentinelEmote.Value;
-                    break;
-                case "Bush Wolf":
-                    return  EnemyInteractionSettings.bushWolfEmote.Value;
-                    break;
-                case "Clay Surgeon":
-                    return  EnemyInteractionSettings.claySurgeonEmote.Value;
-                    break;
-                case "InternNPC":
-                    return EnemyInteractionSettings.internEmote.Value;
-                    break;
-                default:
-                    return false;
-                    break;
-            }
+                "Bunker Spider" => EnemyInteractionSettings.bunkerSpiderEmote.Value,
+                "Hoarding bug" => EnemyInteractionSettings.hoardingBugEmote.Value,
+                "Earth Leviathan" => EnemyInteractionSettings.earthLeviathanEmote.Value,
+                "Crawler" => EnemyInteractionSettings.thumperEmote.Value,
+                "Blob" => EnemyInteractionSettings.hygrodereEmote.Value,
+                "Centipede" => EnemyInteractionSettings.snareFleaEmote.Value,
+                "Nutcracker" => EnemyInteractionSettings.nutcrackerEmote.Value,
+                "Baboon hawk" => EnemyInteractionSettings.baboonHawkEmote.Value,
+                "Puffer" => EnemyInteractionSettings.sporeLizardEmote.Value,
+                "Spring" => EnemyInteractionSettings.coilHeadEmote.Value,
+                "Jester" => EnemyInteractionSettings.jesterEmote.Value,
+                "Flowerman" => EnemyInteractionSettings.brackenEmote.Value,
+                "Girl" => EnemyInteractionSettings.ghostGirlEmote.Value,
+                "MouthDog" => EnemyInteractionSettings.eyelessDogEmote.Value,
+                "ForestGiant" => EnemyInteractionSettings.forestKeeperEmote.Value,
+                "Masked" => EnemyInteractionSettings.maskedEmote.Value,
+                "Shy guy" => EnemyInteractionSettings.shyGuyEmote.Value,
+                "SkibidiToilet" => EnemyInteractionSettings.skibidiToiletEmote.Value,
+                "Demogorgon" => EnemyInteractionSettings.demoGorgonEmote.Value,
+                "Peeper" => EnemyInteractionSettings.peeperEmote.Value,
+                "RadMech" => EnemyInteractionSettings.radMechEmote.Value,
+                "Butler" => EnemyInteractionSettings.butlerEmote.Value,
+                "Tulip Snake" => EnemyInteractionSettings.tulipSnakeEmote.Value,
+                "HarpGhost" => EnemyInteractionSettings.harpGhostEmote.Value,
+                "EnforcerGhost" => EnemyInteractionSettings.enforcerGhostEmote.Value,
+                "BagpipeGhost" => EnemyInteractionSettings.bagpipeGhostEmote.Value,
+                "SlendermanEnemy" => EnemyInteractionSettings.slendermanEmote.Value,
+                "RedWoodGiant" => EnemyInteractionSettings.redWoodGiantEmote.Value,
+                "DriftWoodGiant" => EnemyInteractionSettings.driftWoodGiantEmote.Value,
+                "Foxy" => EnemyInteractionSettings.foxyEmote.Value,
+                "The Fiend" => EnemyInteractionSettings.theFiendEmote.Value,
+                "Siren Head" => EnemyInteractionSettings.sirenHeadEmote.Value,
+                "Football" => EnemyInteractionSettings.footballEmote.Value,
+                "Sentinel" => EnemyInteractionSettings.sentinelEmote.Value,
+                "Bush Wolf" => EnemyInteractionSettings.bushWolfEmote.Value,
+                "Clay Surgeon" => EnemyInteractionSettings.claySurgeonEmote.Value,
+                "InternNPC" => EnemyInteractionSettings.internEmote.Value,
+                "Maneater" => EnemyInteractionSettings.caveDwellerEmote.Value,
+                _ => false,
+            };
         }
     }
 }
